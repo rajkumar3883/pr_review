fac88ea055b7eb970e6a75f37a1f018a5e790aa8
rajverma <rajkumar3883@gmail.com>
2020-06-09 16:11:09 +0530

    new function

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void onView(){
+        i=10;
+    }
+    public void networkRequest(){
+        for(int j=0; j<i; j++){
+            System.out.print("value of j "+j);
+        }
     }
 }
